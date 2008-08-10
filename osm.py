6d17e8b476821f184390b80ad0875e61f1f02f3f
Rory McCann <rory@athena.(none)>
2008-08-10 01:04:23 +0100

    whitespace cleanup

--- osm.py
+++ osm.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python
-import xml.sax
+import xml.sax, math
 
 OSM_API_BASE_URL = "http://api.openstreetmaps.org/api/0.5"
 
@@ -16,6 +16,18 @@ class Node(object):
 
     def __repr__(self):
         return "Node(id=%r, lon=%r, lat=%r, tags=%r)" % (self.id, self.lon, self.lat, self.tags)
+
+    def distance(self, other):
+        """
+        Returns the distance between this point the other in metres
+        """
+        lat1=float(self.lat) * math.pi / 180
+        lat2=float(other.lat) * math.pi / 180
+        lon1=float(self.lon) * math.pi / 180
+        lon2=float(other.lon) * math.pi / 180
+        dist = math.atan(math.sqrt(math.pow(math.cos(lat2)*math.sin(abs(lon1-lon2)),2) + math.pow(math.cos(lat1)*math.sin(lat2) - math.sin(lat1)*math.cos(lat2)*math.cos(lon1-lon2),2)) / (math.sin(lat1)*math.sin(lat2) + math.cos(lat1)*math.cos(lat2)*math.cos(lon1-lon2)))
+        dist *= 6372795 # convert from radians to meters
+        return dist
 
 class Way(object):
     __slots__ = ['id', 'nodes', 'tags']
@@ -34,6 +46,16 @@ class Way(object):
     def __repr__(self):
         return "Way(id=%r, nodes=%r, tags=%r)" % (self.id, self.nodes, self.tags)
 
+    def __len__(self):
+        """
+        Returns the length of the way in metres
+        """
+        if len(self.nodes) < 2:
+            return 0
+        return sum(self.nodes[i].distance(self.nodes[i+1]) for i in range(len(self.nodes)-1))
+
+
+
 class NodePlaceHolder(object):
     __slots__ = ['id']
 
@@ -43,6 +65,15 @@ class NodePlaceHolder(object):
     def __repr__(self):
         return "NodePlaceHolder(id=%r)" % (self.id)
 
+class WayPlaceHolder(object):
+    __slots__ = ['id']
+
+    def __init__(self, id):
+        self.id = id
+
+    def __repr__(self):
+        return "WayPlaceHolder(id=%r)" % (self.id)
+
 class Relation(object):
     __slots__ = ['id', 'roles', 'tags']
 
@@ -51,7 +82,7 @@ class Relation(object):
         self.roles = {}
         self.tags = {}
 
-    def add(item, role=None):
+    def add(self, item, role=None):
         """
         Add the item to this relation with that role. If role is unspecified,
         it's ""
@@ -61,7 +92,7 @@ class Relation(object):
 
         if role not in self.roles:
             self.roles[role] = set()
-        self.roles.add(item)
+        self.roles[role].add(item)
 
 
 
@@ -71,8 +102,9 @@ class OSMXMLFile(object):
 
         self.nodes = {}
         self.ways = {}
+        self.relations = {}
+        self.invalid_ways = []
         self.__parse()
-        #print repr(self.ways)
 
     def __parse(self):
         """Parse the given XML file"""
@@ -81,8 +113,14 @@ class OSMXMLFile(object):
         parser.parse(self.filename)
 
         # now fix up all the refereneces
-        for way in self.ways.values():
-            way.nodes = [self.nodes[node_pl.id] for node_pl in way.nodes]
+        for index, way in self.ways.items():
+            try:
+                way.nodes = [self.nodes[node_pl.id] for node_pl in way.nodes]
+            except KeyError:
+                print "Way (id=%s) referes to a node that doesn't exist, skipping that way" % (index)
+                self.invalid_ways.append(way)
+                del self.ways[index]
+                continue
 
         # convert them back to lists
         self.nodes = self.nodes.values()
@@ -94,6 +132,7 @@ class OSMXMLFileParser(xml.sax.ContentHandler):
         self.containing_obj = containing_obj
         self.curr_node = None
         self.curr_way = None
+        self.curr_relation = None
 
     def startElement(self, name, attrs):
         #print "Start of node " + name
@@ -102,17 +141,28 @@ class OSMXMLFileParser(xml.sax.ContentHandler):
         elif name == 'way':
             #self.containing_obj.ways.append(Way())
             self.curr_way = Way(id=attrs['id'])
+        elif name == 'relation':
+            self.curr_relation = Relation(id=attrs['id'])
         elif name == 'tag':
             #assert not self.curr_node and not self.curr_way, "curr_node (%r) and curr_way (%r) are both non-None" % (self.curr_node, self.curr_way)
-            if self.curr_node:
+            if self.curr_node is not None:
                 self.curr_node.tags[attrs['k']] = attrs['v']
-            elif self.curr_way:
+            elif self.curr_way is not None:
                 self.curr_way.tags[attrs['k']] = attrs['v']
         elif name == "nd":
             assert self.curr_node is None, "curr_node (%r) is non-none" % (self.curr_node)
             assert self.curr_way is not None, "curr_way is None"
             self.curr_way.nodes.append(NodePlaceHolder(id=attrs['ref']))
-        elif name == "osm":
+        elif name == "member":
+            #import pdb ; pdb.set_trace()
+            assert self.curr_relation is not None, "<member> tag and no relation"
+            if attrs['type'] == 'way':
+                self.curr_relation.add(WayPlaceHolder(id=attrs['ref']), role=attrs['role'])
+            elif attrs['type'] == 'node':
+                self.curr_relation.add(NodePlaceHolder(id=attrs['ref']), role=attrs['role'])
+            else:
+                assert False, "Unknown member type "+repr(attrs['type'])
+        elif name in ["osm", "bounds"]:
             pass
         else:
             print "Unknown node: "+name
@@ -128,14 +178,46 @@ class OSMXMLFileParser(xml.sax.ContentHandler):
             self.containing_obj.ways[self.curr_way.id] = self.curr_way
             self.curr_way = None
 
+class GPSData(object):
+    """
+    Downloads data GPS track data from OpenStreetMap Server
+    """
+    def __init__(self, left, bottom, right, top):
+        self.left = left
+        self.bottom = bottom
+        self.right = right
+        self.top = top
+        self.tracks = []
+
+    def __parse_file(filename):
+        parser = xml.sax.make_parser()
+        parser.setContentHandler(GPXParser())
+        parser.parse(filename)
+
+        print "Obtained %d tracks from file %s" % (len(self.tracks), filename)
+
+class GPXParser(xml.sax.ContentHandler):
+    """
+    Parses GPX files from the OSM GPS trackpoint downloader. Converts them to OSM format
+    """
+    def __init__(self, containing_obj):
+        self.tracks = []
+        self.__current_way = None
+        self.containing_obj = containing_obj
+
+    def startElement(self, name, attrs):
+        if name == "trkseg":
+            self.__current_way = Way()
+        elif name == "trkpt":
+            assert self.__current_way is not None, "Invalid GPX file, we've encountered a trkpt tag before a trkseg tag"
+            self.__current_way.nodes.append(Node(lat=attrs['lat'], lon=attrs['lon']))
+
+    def endElement(self, name):
+        self.containing_obj.tracks.append(self.__current_way)
+
+
+
 def api_get_node(node_id):
     url = "%s/node/%s" % (OSM_API_BASE_URL, node_id)
     print url
-
-if __name__ == '__main__':
-    osm = OSMXMLFile("./lucan.osm.xml")
-    import code
-    code.interact(local=locals())
-
-
 
