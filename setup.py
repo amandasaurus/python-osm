02785f1993a493fd97db6b5b2f8f0719c53d352e
Rory McCann <rory@technomancy.org>
2014-10-18 16:57:35 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(name="python-osm",
+      version="0.5",
+      author="Rory McCann",
+      author_email="rory@technomancy.org",
+      py_modules=['popline'],
+      license = 'GPLv3+',
+      description = "Python OSM",
+      classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+      ],
+)
